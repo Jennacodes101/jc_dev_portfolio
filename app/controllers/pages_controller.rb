abf3fcebd1a5fb989c0e95de9c6c5f268cc0e8e0
Jenna Castillo <jennacodes101@gmail.com>
2022-10-20 21:36:26 -0500

    added style

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def download_pdf
+  send_file(
+    "#{Rails.root}/public/Jenna_Castillo_full_stack.pdf",
+    filename: "Jenna_Castillo.pdf",
+    type: "application/pdf"
+  )
+end
 end
